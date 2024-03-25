980db68b4f001ecfe22319d04dad218bbfa8a284
avisek123 <sahooavisek41@gmail.com>
2024-03-25 12:10:29 +0530

    minor changes done

--- src/services/apiSlice.ts
+++ src/services/apiSlice.ts
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import {BASE_URL} from 'configs';
-
 import {logOut, setCredentials} from 'features';
 import {RootState} from 'src/redux/Store';
 import {getItem, setItem} from 'utils';
